d42379cbd2dfebea92ca643d5b66e4f4f6679e5f
Junhyunny <kang3966@naver.com>
2026-01-09 15:07:55 +0900

    Fix chapter 05 클래스 이름 변경

--- part-02/chapter-05/demo/src/main/java/com/example/demo/controller/SignUpController.java
+++ part-02/chapter-05/demo/src/main/java/com/example/demo/controller/SignUpController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
-import com.example.demo.domain.model.Signup;
-import com.example.demo.service.SignupService;
+import com.example.demo.domain.model.SignUp;
+import com.example.demo.service.SignUpService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,19 +9,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class SignUpController {
 
-    private final SignupService signupService;
+    private final SignUpService signupService;
 
-    public SignUpController(SignupService signupService) {
+    public SignUpController(SignUpService signupService) {
         this.signupService = signupService;
     }
 
+    /* [1] 회원 가입 페이지를 반환 */
     @GetMapping("/signup")
     public String signup() {
         return "signup";
     }
 
+    /* [2] 회원 가입 요청 처리 */
     @PostMapping("/signup")
-    public String signup(Signup requestBody) {
+    public String signup(SignUp requestBody) {
         signupService.signup(requestBody);
         return "redirect:/login";
     }
--- part-02/chapter-05/demo/src/main/java/com/example/demo/service/SignupService.java
+++ part-02/chapter-05/demo/src/main/java/com/example/demo/service/SignupService.java
@@ -1,23 +1,25 @@
 package com.example.demo.service;
 
-import com.example.demo.domain.model.Signup;
+import com.example.demo.domain.model.SignUp;
 import com.example.demo.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class SignupService {
+public class SignUpService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public SignupService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public SignUpService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public void signup(Signup requestBody) {
-        var user = requestBody.toEntity(passwordEncoder::encode); // 1
-        userRepository.save(user); // 2
+    public void signup(SignUp requestBody) {
+        /* [1] 사용자 엔티티 객체 생성 */
+        var user = requestBody.toEntity(passwordEncoder::encode);
+        /* [2] 사용자 정보를 데이터베이스에 영속화 */
+        userRepository.save(user);
     }
 }
